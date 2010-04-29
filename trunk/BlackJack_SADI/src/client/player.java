68fde1c77ac77161f4af336808502b55562e2772
sushyan <sushyan@9d4d097a-e9aa-41cb-ce4f-9eb0714609bd>
2010-04-29 00:49:22 +0000

    skeleton of player

---
+++ trunk/BlackJack_SADI/src/client/player.java
@@ -0,0 +1,39 @@
+package client;
+
+import server.*;
+import utility.*;
+
+public class player extends BlackJackHand
+{
+	private String username;
+	private int playerTurn;
+	private boolean isMyTurn;
+	
+	public player(String username, int playerTurn) 
+	{
+		this.username = username;
+		this.playerTurn = playerTurn;
+		isMyTurn = false;
+	}
+	
+	public void deal()
+	{
+		
+	}
+	
+	public void hit()
+	{
+		
+	}
+	
+	public void stand()
+	{
+		
+	}
+		
+	public void split()
+	{
+		
+	}
+
+}
